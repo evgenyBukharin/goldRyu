42d38b053006390e54fbcbe9ca127802e3538c31
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-03-16 16:37:02 +0500

    yandex maps temp vigdet

--- src/js/components/yandex-map.js
+++ src/js/components/yandex-map.js
@@ -1,8 +1,8 @@
-ymaps.ready(init);
+// ymaps.ready(init);
 
-function init() {
-	myMap = new ymaps.Map("contacts-map", {
-		center: [55.17335456952944, 61.3061735],
-		zoom: 17,
-	});
-}
+// function init() {
+// 	myMap = new ymaps.Map("contacts-map", {
+// 		center: [55.17335456952944, 61.3061735],
+// 		zoom: 17,
+// 	});
+// }
