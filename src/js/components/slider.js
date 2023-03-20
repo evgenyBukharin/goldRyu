44b85a7ad34e26937b7e44724356a09a17d8d194
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-03-20 10:47:38 +0500

    worst slider done

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,5 +1,23 @@
-import Swiper, { Pagination } from "swiper";
-Swiper.use([Pagination]);
+import Swiper, { Pagination, EffectCreative } from "swiper";
+Swiper.use([Pagination, EffectCreative]);
 const swiper = new Swiper(document.querySelector(".swiper"), {
 	slidesPerView: "auto",
+	spaceBetween: 40,
+	resistanceRatio: 0,
+	speed: 400,
+	effect: "creative",
+	creativeEffect: {
+		prev: {
+			// will set `translateZ(-400px)` on previous slides
+			translate: [0, 0, -100],
+		},
+		next: {
+			// will set `translateX(100%)` on next slides
+			translate: ["100%", 0, 0],
+		},
+	},
+	pagination: {
+		el: ".main-hero__pagination",
+		type: "progressbar",
+	},
 });
