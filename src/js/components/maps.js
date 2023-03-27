cf15ad9b6a33aed8ee3538eac140d6ef79e65c06
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-03-27 16:59:37 +0500

    api key

--- src/js/components/maps.js
+++ src/js/components/maps.js
@@ -1,29 +1,29 @@
-// ymaps.ready(init);
-// function init() {
-// 	if (document.getElementById("contacts-map") !== null) {
-// 		myMap = new ymaps.Map("contacts-map", {
-// 			center: [55.05386257036589, 61.45853671216136],
-// 			zoom: 9,
-// 		});
-// 	} else if (document.getElementById("main-map") !== null) {
-// 		myMap = new ymaps.Map("main-map", {
-// 			center: [55.05386257036589, 61.45853671216136],
-// 			zoom: 9,
-// 		});
-// 	} else {
-// 		return;
-// 	}
-// 	const geoObject1 = new ymaps.GeoObject({
-// 		geometry: {
-// 			type: "Point",
-// 			coordinates: [55.154938569541265, 61.31335099999995],
-// 		},
-// 	});
-// 	const geoObject2 = new ymaps.GeoObject({
-// 		geometry: {
-// 			type: "Point",
-// 			coordinates: [54.86839356980941, 61.40452099999999],
-// 		},
-// 	});
-// 	myMap.geoObjects.add(geoObject1).add(geoObject2);
-// }
+ymaps.ready(init);
+function init() {
+	if (document.getElementById("contacts-map") !== null) {
+		myMap = new ymaps.Map("contacts-map", {
+			center: [55.05386257036589, 61.45853671216136],
+			zoom: 9,
+		});
+	} else if (document.getElementById("main-map") !== null) {
+		myMap = new ymaps.Map("main-map", {
+			center: [55.05386257036589, 61.45853671216136],
+			zoom: 9,
+		});
+	} else {
+		return;
+	}
+	const geoObject1 = new ymaps.GeoObject({
+		geometry: {
+			type: "Point",
+			coordinates: [55.154938569541265, 61.31335099999995],
+		},
+	});
+	const geoObject2 = new ymaps.GeoObject({
+		geometry: {
+			type: "Point",
+			coordinates: [54.86839356980941, 61.40452099999999],
+		},
+	});
+	myMap.geoObjects.add(geoObject1).add(geoObject2);
+}
