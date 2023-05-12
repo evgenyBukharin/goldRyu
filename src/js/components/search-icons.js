ee7a7bd351b27aa360998ac37fd2040ab9414588
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-05-12 10:29:30 +0500

    search scroll block

--- src/js/components/search-icons.js
+++ src/js/components/search-icons.js
@@ -3,18 +3,25 @@ const searchIcons = document.querySelectorAll(".search-icon");
 const searchContent = document.querySelector(".search__content");
 const burger = document.getElementById("burger");
 const menuBg = document.querySelector(".menu__bg");
+const body = document.querySelector("body");
 
 if (searchIcons !== null && searchSection !== null && searchContent !== null) {
 	searchIcons.forEach((icon) => {
 		icon.addEventListener("click", () => {
 			if (searchSection.classList.contains("search-visible")) {
 				searchSection.classList.remove("search-visible");
+				body.style.overflowY = "scroll";
+				body.style.paddingRight = "0";
+				header.style.paddingRight = "0";
 			} else {
 				if (burger !== null && menuBg !== null) {
 					menuBg.classList.remove("menu__bg-active");
 					burger.classList.remove("burger--active");
 				}
 				setTimeout(() => {
+					body.style.overflowY = "hidden";
+					body.style.paddingRight = "15px";
+					header.style.paddingRight = "15px";
 					searchSection.classList.add("search-visible");
 				}, 300);
 			}
@@ -22,6 +29,9 @@ if (searchIcons !== null && searchSection !== null && searchContent !== null) {
 	});
 	searchSection.addEventListener("click", () => {
 		searchSection.classList.remove("search-visible");
+		body.style.overflowY = "scroll";
+		body.style.paddingRight = "0";
+		header.style.paddingRight = "0";
 	});
 	searchContent.addEventListener("click", (e) => {
 		e.stopPropagation();
