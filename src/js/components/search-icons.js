fdba1f0b6091a979f6dbdf66e50d26c16e4a7ed9
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-05-12 10:50:05 +0500

    + mobile check

--- src/js/components/search-icons.js
+++ src/js/components/search-icons.js
@@ -5,33 +5,41 @@ const burger = document.getElementById("burger");
 const menuBg = document.querySelector(".menu__bg");
 const body = document.querySelector("body");
 
+import { mobileCheck } from "../functions/mobile-check";
+
 if (searchIcons !== null && searchSection !== null && searchContent !== null) {
 	searchIcons.forEach((icon) => {
 		icon.addEventListener("click", () => {
 			if (searchSection.classList.contains("search-visible")) {
-				searchSection.classList.remove("search-visible");
+				if (mobileCheck() == "unknown") {
+					body.style.paddingRight = "0";
+					header.style.paddingRight = "0";
+				}
 				body.style.overflowY = "scroll";
-				body.style.paddingRight = "0";
-				header.style.paddingRight = "0";
+				searchSection.classList.remove("search-visible");
 			} else {
 				if (burger !== null && menuBg !== null) {
 					menuBg.classList.remove("menu__bg-active");
 					burger.classList.remove("burger--active");
 				}
 				setTimeout(() => {
+					if (mobileCheck() == "unknown") {
+						body.style.paddingRight = "15px";
+						header.style.paddingRight = "15px";
+					}
 					body.style.overflowY = "hidden";
-					body.style.paddingRight = "15px";
-					header.style.paddingRight = "15px";
 					searchSection.classList.add("search-visible");
 				}, 300);
 			}
 		});
 	});
 	searchSection.addEventListener("click", () => {
-		searchSection.classList.remove("search-visible");
+		if (mobileCheck() == "unknown") {
+			body.style.paddingRight = "0";
+			header.style.paddingRight = "0";
+		}
 		body.style.overflowY = "scroll";
-		body.style.paddingRight = "0";
-		header.style.paddingRight = "0";
+		searchSection.classList.remove("search-visible");
 	});
 	searchContent.addEventListener("click", (e) => {
 		e.stopPropagation();
