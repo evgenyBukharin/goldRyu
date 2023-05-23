832d73b390d328001cd6303f9f700f76ba3488a1
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-05-23 14:08:42 +0500

    правильные отступы + изображение на странце о компании + текст в баннере там же

--- src/js/components/links-simulation.js
+++ src/js/components/links-simulation.js
@@ -1,6 +1,14 @@
 const linkItems = document.querySelectorAll("[data-href]");
 if (linkItems !== null) {
 	linkItems.forEach((link) => {
+		const innerButtons = link.querySelectorAll("button");
+
+		innerButtons.forEach((btn) => {
+			btn.addEventListener("click", (event) => {
+				event.stopPropagation();
+			});
+		});
+
 		link.addEventListener("click", () => {
 			const href = link.getAttribute("data-href");
 			document.location.href = href;
